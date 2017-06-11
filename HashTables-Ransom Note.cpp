b6c8a649c807c6f0ad0ae3b17ce73417122d46e7
Monica <lalaphoon@gmail.com>
2017-06-10 20:30:34 -0400

    Add them

---
+++ HashTables-Ransom Note.cpp
@@ -0,0 +1,64 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) {
+    unordered_map<string, int> hashtable;
+    for(int i = 0; i < magazine.size();i++){
+        hashtable[magazine[i]]++;
+    }
+    for (int i = 0; i < ransom.size(); i++){
+        if (hashtable.count(ransom[i])==0){
+            return false;
+        }
+        else {
+            hashtable[ransom[i]]--;
+            if (hashtable[ransom[i]]<0){
+                return false;
+            }
+            
+        }
+    }
+    return true;
+}
+
+
+int main(){
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
